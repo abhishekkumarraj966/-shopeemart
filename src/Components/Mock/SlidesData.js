1b5cc1a65dbf9bdb73d412b8e53b73ecb80ba7fb
abhishekkumarraj966 <abhishekkumarraj62073@gmail.com>
2024-04-30 14:20:24 +0530

    sdfghj

--- src/Components/Mock/SlidesData.js
+++ src/Components/Mock/SlidesData.js
@@ -1,37 +1,79 @@
 const slidesData = [
-    {
-      id: 1,
-     
-      title: '24x7 Service',
-      description: 'Lorem ipsum dolor sit amet consectetur elit.',
-    },
-    {
-        id: 1,
-       
-        title: '100% secure payment',
-        description: 'Lorem ipsum dolor sit amet consectetur elit.',
-      },
-   
-      {
-        id: 1,
-     
-        title: 'Quality guarantee',
-        description: 'Lorem ipsum dolor sit amet consectetur elit.',
-      },
-   
-      {
-        id: 1,
-       
-        title: 'guaranteed savings',
-        description: 'Lorem ipsum dolor sit amet consectetur elit.',
-      },
-   
-      {
-        id: 1,
-      
-        title: 'Daily offers',
-        description: 'Lorem ipsum dolor sit amet consectetur elit.',
-      }
-     
-  ];
-  export default slidesData;
+  {
+    id: 1,
+
+    title: "Listing Website",
+    description: ` Boost your online presence with Bizzata's dynamic listing website. Showcase your business and attract more customers.,`,
+  },
+  {
+    id: 1,
+
+    title: "Product Showcase",
+    description: `Display your products with flair using our elegant showcase feature. Make a lasting impression on potential buyers.`,
+  },
+
+  {
+    id: 1,
+
+    title: "Appointment Booking",
+    description: `Streamline your schedule with our convenient appointment booking system. Simplify client bookings and optimize your time.`,
+  },
+
+  {
+    id: 1,
+
+    title: "Services Display:",
+    description: `Present your services professionally with Bizzata. Highlight your expertise and attract clients effortlessly.
+        `,
+  },
+
+  {
+    id: 1,
+
+    title: "Offers Promotion:",
+    description: `Attract customers with irresistible offers using our promotional tools. Drive sales and boost revenue with targeted promotions.
+        `,
+  },
+
+  {
+    id: 1,
+
+    title: "Photo Gallery:",
+    description: `Tell your story visually with our stunning photo gallery. Showcase your work and captivate your audience.`,
+  },
+  {
+    id: 1,
+
+    title: "10 Attractive Templates:",
+    description: `Choose from our selection of 10 attractive templates to create a website that reflects your brand identity. Stand out from the crowd with a unique design.
+        `,
+  },
+  {
+    id: 1,
+
+    title: "Google Business Ranking:",
+    description: `Improve your visibility on Google with Bizzata's business ranking feature. Increase your online presence and attract more customers.
+        `,
+  },
+
+  {
+    id: 1,
+
+    title: "Social Media Integration:",
+    description: `Expand your reach with seamless social media integration. Connect with your audience across multiple platforms and build your online community.`,
+  },
+  {
+    id: 1,
+
+    title: "Lead Contact Details",
+    description: `Capture valuable leads with our lead contact details feature. Stay in touch with potential customers and nurture relationships for future growth.
+        `,
+  },
+  {
+    id: 1,
+
+    title: "NFC Business Card:",
+    description: `Make networking effortless with our NFC business card. Share your contact details with a simple tap, and make a lasting impression on clients and prospects.`,
+  },
+];
+export default slidesData;
--- src/Components/Shoopmartgivyou.jsx
+++ src/Components/Shoopmartgivyou.jsx
@@ -13,7 +13,7 @@ const Shoopmartgivyou = () => {
     <AppLayout>
       <div className="my-10">
         <h1 className="text-center font-bold text-4xl text-[#000] tracking-1 sm:tracking-2 font-serif mb-8">
-          ShopeeMart Gives You
+        Bizzata gives you
         </h1>
 
         <Swiper
@@ -30,10 +30,7 @@ const Shoopmartgivyou = () => {
               slidesPerView: 3,
               spaceBetween: 15,
             },
-            770: {
-              slidesPerView: 4,
-              spaceBetween: 10,
-            },
+            
           }}
           spaceBetween={30}
           centeredSlides={true}
@@ -52,15 +49,15 @@ const Shoopmartgivyou = () => {
             {slidesData.map((slide) => (
               <SwiperSlide key={slide.id}>
                 <div className="flex gap-4 pb-10">
-                  <div className="h-[100px] sm:w-[300px] w-[320px] mx-auto shadow-md flex gap-1">
+                  <div className="h-[120px] sm:w-[350px] w-[320px] mx-auto shadow-md flex gap-1">
                     <div className="pt-1">
                       <img src={card} alt="" />
                     </div>
                     <div className="pt-2">
-                      <h1 className="font-bold text-20px cursor-pointer">
+                      <h1 className="font-bold text-[16px] cursor-pointer">
                         <a href="">{slide.title}</a>
                       </h1>
-                      <p className="text-14px mx-2 font-normal">
+                      <p className="text-[13px] mx-2 font-normal">
                         {slide.description}
                       </p>
                     </div>
