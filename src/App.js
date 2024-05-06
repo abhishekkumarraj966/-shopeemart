33abdc6bf7b3bca5bebb1f5be69594c2d95ef0c3
abhishekkumarraj966 <abhishekkumarraj62073@gmail.com>
2024-05-06 10:47:10 +0530

    sdfghj

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import "./App.css";
-import Category from "./Components/Category";
-import Customberp from "./Components/Customberp";
-import Feedback from "./Components/Feedback";
 import Footer from "./Components/Footer";
-import GetTouch from "./Components/GetTouch";
-import Hero from "./Components/Hero";
-import HowItWorks from "./Components/HowItWorks";
+
+import Layout from "./Components/Layout";
 import Navbar from "./Components/Navbar";
-import Pricing from "./Components/Pricing";
-import Recentblg from "./Components/Recentblg";
+
 import Searchpag from "./Components/Searchpag";
-import Shoopmartgivyou from "./Components/Shoopmartgivyou";
-import Shopymart from "./Components/Shopymart";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      {/* <Hero />
-      <Customberp />
-      <Shopymart />
-      <Shoopmartgivyou />
-      <Category />
-      <HowItWorks/>      
-      <Pricing />
-      <Feedback />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Layout />} />
 
-      <Recentblg /> */}
-      {/* <GetTouch /> */}
-      <Searchpag/>
-      <Footer />
+          <Route path="/searchpg" element={<Searchpag />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Layout.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Hero from "./Hero";
+import Customberp from "./Customberp";
+import Shopymart from "./Shopymart";
+import Shoopmartgivyou from "./Shoopmartgivyou";
+import Category from "./Category";
+import HowItWorks from "./HowItWorks";
+import Pricing from "./Pricing";
+import Feedback from "./Feedback";
+import GetTouch from "./GetTouch";
+import Recentblg from "./Recentblg";
+const Layout = () => {
+  return (
+    <div className="bg-[#ECECEC]">
+      <Hero />
+      <Customberp />
+      <Shopymart />
+      <Shoopmartgivyou />
+      <Category />
+      <HowItWorks />
+      <Pricing />
+      <Feedback />
+
+      <Recentblg />
+      <GetTouch />
+    </div>
+  );
+};
+
+export default Layout;
